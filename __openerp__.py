39eb208c59f0fdb55f9b401c77ee4518a0496730
Yusnel Rojas Garcia <yrojass@gmail.com>
2015-02-03 15:01:53 -0600

    website integration

--- __openerp__.py
+++ __openerp__.py
@@ -33,7 +33,7 @@ This module aims to help in the development of new modules
     'author': 'Soluciones Moebius',
     #"license": "AGPL-3",
     'website': 'http://www.solucionesmoebius.com/',
-    'depends': ['web', 'base_mixins', 'web_diagram', 'generic_document_render_jinja', 'web_ace_editor'],
+    'depends': ['web', 'base_mixins', 'web_diagram', 'generic_document_render_jinja', 'web_ace_editor', 'website'],
     'data': [
         # 'security/base_security.xml',
         # 'security/ir.model.access.csv',
